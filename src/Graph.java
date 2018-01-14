148df88c0d05cf7d8c61ebea1c8874e840105475
Taylor J. Dawson <taylorjdawson@gmail.com>
2018-01-14 07:31:16 -0800

    .gitignore updated

--- src/Graph.java
+++ src/Graph.java
@@ -2,16 +2,39 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class Graph<T> {
-   public Graph(boolean isDirected) {}
-   public List<T> getVertices() {}
-   public int getEdgeWeight(T source, T destination) {}
-   public boolean edgeExists(T source, T destination) {}
-   public void addVertex(T vertex) throws IllegalArgumentException {}
-   public void removeVertex(T vertex) throws NoSuchElementException {}
-   public void addEdge(T source, T destination, int weight)
-         throws IllegalArgumentException, NoSuchElementException {}
-   public void removeEdge(T source, T destination) throws
-         NoSuchElementException {}
-   public String toString() {}
+
+    public Graph(boolean isDirected) {
+    }
+
+    public List<T> getVertices() {
+        return null;
+    }
+
+    public int getEdgeWeight(T source, T destination) {
+        return 0;
+    }
+
+    public boolean edgeExists(T source, T destination) {
+        return false;
+    }
+
+    public void addVertex(T vertex) throws IllegalArgumentException {
+    }
+
+    public void removeVertex(T vertex) throws NoSuchElementException {
+    }
+
+    public void addEdge(T source, T destination, int weight)
+            throws IllegalArgumentException, NoSuchElementException {
+    }
+
+    public void removeEdge(T source, T destination) throws
+            NoSuchElementException {
+    }
+
+    public String toString() {
+        return null;
+    }
 }
+
 //TODO: Implement using map of maps
