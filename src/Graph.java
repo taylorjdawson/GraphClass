21faebf4220d377f742f38e754948de37711b5c7
Taylor J. Dawson <taylorjdawson@gmail.com>
2018-01-16 11:50:33 -0800

    W.I.P

--- src/Graph.java
+++ src/Graph.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 public class Graph<T> {
 
+    private static boolean _isDirected;
+
+    private HashMap<T, HashMap<T, Integer>> adjacencyList;
+
     public Graph(boolean isDirected) {
+        _isDirected = isDirected;
+        adjacencyList = new HashMap<>();
     }
 
     public List<T> getVertices() {
-        return null;
+        return new ArrayList<T>(adjacencyList.keySet());
     }
 
     public int getEdgeWeight(T source, T destination) {
@@ -19,13 +27,22 @@ public class Graph<T> {
     }
 
     public void addVertex(T vertex) throws IllegalArgumentException {
+        adjacencyList.put(vertex, new HashMap<>());
     }
 
     public void removeVertex(T vertex) throws NoSuchElementException {
+
+        //TODO: Check that the vertex exists
+        adjacencyList.remove(vertex);
     }
 
     public void addEdge(T source, T destination, int weight)
             throws IllegalArgumentException, NoSuchElementException {
+
+        //TODO: Check that edges don't already exist
+
+        //TODO: Check that the vertex exists
+        adjacencyList.get(source).put(destination, weight);
     }
 
     public void removeEdge(T source, T destination) throws
